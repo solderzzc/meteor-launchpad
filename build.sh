529be1acbb468a028d033a3940d7e4784a011c82
Simba Zhang <solderzzc@gmail.com>
2019-04-20 14:36:40 -0700

    To not build devbuild.

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ IMAGE_NAME=${1:-"jshimko/meteor-launchpad"}
 
 printf "\n[-] Building $IMAGE_NAME...\n\n"
 
-docker build -f dev.dockerfile -t $IMAGE_NAME:devbuild .
-docker build -t $IMAGE_NAME:latest .
+#docker build -f dev.dockerfile -t $IMAGE_NAME:devbuild .
+docker build -t $IMAGE_NAME .
